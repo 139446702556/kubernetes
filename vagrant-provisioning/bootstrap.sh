94e843c66768fbfca90187fa02a079a62f37e68e
zhan-yl <zhan_yl@163.com>
2020-07-29 21:23:24 +0800

    Update bootstrap.sh
    Update yum repo for docker installation

--- vagrant-provisioning/bootstrap.sh
+++ vagrant-provisioning/bootstrap.sh
@@ -23,7 +23,7 @@ yum makecache >/dev/null 2>&1
 yum repolist >/dev/null 2>&1
 
 yum install -y -q yum-utils device-mapper-persistent-data lvm2 > /dev/null 2>&1
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null 2>&1
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo > /dev/null 2>&1
 yum install -y -q docker-ce >/dev/null 2>&1
 
 # Enable docker service
